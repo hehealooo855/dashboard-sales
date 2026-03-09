abadf517743bc4921a503d48771e96f0b048f4db
hehealooo855 <hehealooo855@gmail.com>
2026-03-09 11:07:19 +0700

    app.py

--- app.py
+++ app.py
@@ -24,9 +24,6 @@ try:
 except ImportError:
     AGGRID_AVAILABLE = False
 
-# --- ROADMAP 1: IMPORT KONFIGURASI DARI FILE LAIN (CLEAN CODE) ---
-from config import TARGET_DATABASE, INDIVIDUAL_TARGETS, SUPERVISOR_TOTAL_TARGETS, TARGET_NASIONAL_VAL, BRAND_ALIASES, SALES_MAPPING
-
 # --- 1. KONFIGURASI HALAMAN & CSS PREMIUM ---
 st.set_page_config(
     page_title="Dashboard Sales", 
@@ -60,13 +57,194 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # ==========================================
+# 2. KONFIGURASI DATABASE & TARGET (DIKEMBALIKAN KE FILE UTAMA)
+# ==========================================
+
+TARGET_DATABASE = {
+    "MADONG": {
+        "Somethinc": 1_200_000_000, 
+        "SYB": 150_000_000, 
+        "Sekawan": 600_000_000, # AINIE
+        "Avione": 300_000_000, 
+        "Honor": 125_000_000, 
+        "Vlagio": 75_000_000,
+        "Ren & R & L": 20_000_000, 
+        "Mad For Make Up": 25_000_000, 
+        "Satto": 500_000_000,
+        "Mykonos": 20_000_000
+    },
+    "LISMAN": {
+        "Javinci": 1_300_000_000, 
+        "Careso": 400_000_000, 
+        "Newlab": 150_000_000, 
+        "Gloow & Be": 130_000_000, # Glowbe
+        "Dorskin": 20_000_000, 
+        "Whitelab": 150_000_000, 
+        "Bonavie": 50_000_000, 
+        "Goute": 50_000_000, 
+        "Mlen": 100_000_000, 
+        "Artist Inc": 130_000_000
+    },
+    "AKBAR": {
+        "Sociolla": 600_000_000, 
+        "Thai": 300_000_000, 
+        "Inesia": 100_000_000, 
+        "Y2000": 180_000_000, 
+        "Diosys": 520_000_000,
+        "Masami": 40_000_000, 
+        "Cassandra": 50_000_000, 
+        "Clinelle": 80_000_000
+    },
+    "WILLIAM": {
+        "The Face": 600_000_000, 
+        "Yu Chun Mei": 450_000_000, 
+        "Milano": 50_000_000, 
+        "Remar": 0,
+        "Beautica": 100_000_000, 
+        "Walnutt": 30_000_000, 
+        "Elizabeth Rose": 50_000_000, 
+        "Maskit": 30_000_000, 
+        "Claresta": 350_000_000, 
+        "Birth Beyond": 120_000_000, 
+        "OtwooO": 200_000_000, 
+        "Rose All Day": 50_000_000
+    }
+}
+
+INDIVIDUAL_TARGETS = {
+    "WIRA": { 
+        "Somethinc": 660_000_000, "SYB": 75_000_000, "Honor": 37_500_000, "Vlagio": 22_500_000,
+        "Elizabeth Rose": 30_000_000, "Walnutt": 20_000_000
+    },
+    "HAMZAH": { 
+        "Somethinc": 540_000_000, "SYB": 75_000_000, "Sekawan": 60_000_000, 
+        "Avione": 60_000_000, "Honor": 37_500_000, "Vlagio": 22_500_000 
+    },
+    "ROZY": { "Sekawan": 100_000_000, "Avione": 100_000_000 },
+    "NOVI": { "Sekawan": 90_000_000, "Avione": 90_000_000 },
+    "DANI": { "Sekawan": 50_000_000, "Avione": 50_000_000 },
+    "FERI": { "Honor": 50_000_000, "Thai": 200_000_000, "Vlagio": 30_000_000, "Inesia": 30_000_000 },
+    "NAUFAL": { "Javinci": 550_000_000 },
+    "RIZKI": { "Javinci": 450_000_000 },
+    "ADE": { 
+        "Javinci": 180_000_000, "Careso": 20_000_000, "Newlab": 75_000_000, 
+        "Gloow & Be": 60_000_000, "Dorskin": 10_000_000, "Mlen": 50_000_000 
+    },
+    "FANDI": { 
+        "Javinci": 40_000_000, "Careso": 20_000_000, "Newlab": 75_000_000, 
+        "Gloow & Be": 60_000_000, "Dorskin": 10_000_000, "Whitelab": 75_000_000,
+        "Goute": 25_000_000, "Bonavie": 25_000_000, "Mlen": 50_000_000
+    },
+    "SYAHRUL": { "Javinci": 40_000_000, "Careso": 10_000_000, "Gloow & Be": 10_000_000 },
+    "RISKA": { 
+        "Javinci": 40_000_000, "Sociolla": 190_000_000, "Thai": 30_000_000, "Inesia": 20_000_000 
+    },
+    "DWI": { "Careso": 350_000_000 },
+    "SANTI": { "Goute": 25_000_000, "Bonavie": 25_000_000, "Whitelab": 75_000_000 },
+    "ASWIN": { "Artist Inc": 130_000_000 },
+    "DEVI": { "Sociolla": 120_000_000, "Y2000": 65_000_000, "Diosys": 175_000_000 },
+    "GANI": { 
+        "The Face": 200_000_000, "Yu Chun Mei": 175_000_000, "Milano": 20_000_000,
+        "Sociolla": 80_000_000, "Thai": 85_000_000, "Inesia": 25_000_000
+    },
+    "BASTIAN": { 
+        "Sociolla": 210_000_000, "Thai": 85_000_000, "Inesia": 25_000_000,
+        "Y2000": 65_000_000, "Diosys": 175_000_000
+    },
+    "BAYU": { "Y2000": 50_000_000, "Diosys": 170_000_000 },
+    "YOGI": { "The Face": 400_000_000, "Yu Chun Mei": 275_000_000, "Milano": 30_000_000 },
+    "LYDIA": { "Birth Beyond": 120_000_000 },
+    "MITHA": { 
+        "Maskit": 30_000_000, "Rose All Day": 30_000_000, 
+        "OtwooO": 200_000_000, "Claresta": 350_000_000 
+    }
+}
+
+SUPERVISOR_TOTAL_TARGETS = {k: sum(v.values()) for k, v in TARGET_DATABASE.items()}
+TARGET_NASIONAL_VAL = sum(SUPERVISOR_TOTAL_TARGETS.values())
+
+BRAND_ALIASES = {
+    "Diosys": ["DIOSYS", "DYOSIS", "DIO"], "Y2000": ["Y2000", "Y 2000", "Y-2000"],
+    "Masami": ["MASAMI", "JAYA"], "Cassandra": ["CASSANDRA", "CASANDRA"],
+    "Thai": ["THAI"], "Inesia": ["INESIA"], "Honor": ["HONOR"], "Vlagio": ["VLAGIO"],
+    "Sociolla": ["SOCIOLLA"], "Skin1004": ["SKIN1004", "SKIN 1004"], "Oimio": ["OIMIO"],
+    "Clinelle": ["CLINELLE", "CLIN"], "Ren & R & L": ["REN", "R & L", "R&L"], "Sekawan": ["SEKAWAN", "AINIE"],
+    "Mad For Make Up": ["MAD FOR", "MAKE UP", "MAJU", "MADFORMAKEUP"], "Avione": ["AVIONE"],
+    "SYB": ["SYB"], "Satto": ["SATTO"], "Liora": ["LIORA"], "Mykonos": ["MYKONOS"],
+    "Somethinc": ["SOMETHINC"], "Gloow & Be": ["GLOOW", "GLOOWBI", "GLOW", "GLOWBE"],
+    "Artist Inc": ["ARTIST", "ARTIS"], "Bonavie": ["BONAVIE"], "Whitelab": ["WHITELAB"],
+    "Goute": ["GOUTE"], "Dorskin": ["DORSKIN"], "Javinci": ["JAVINCI"], "Madam G": ["MADAM", "MADAME"],
+    "Careso": ["CARESO"], "Newlab": ["NEWLAB"], "Mlen": ["MLEN"], "Walnutt": ["WALNUT", "WALNUTT"],
+    "Elizabeth Rose": ["ELIZABETH"], "OtwooO": ["OTWOOO", "O.TWO.O", "O TWO O"],
+    "Saviosa": ["SAVIOSA"], "The Face": ["THE FACE", "THEFACE"], "Yu Chun Mei": ["YU CHUN MEI", "YCM"],
+    "Milano": ["MILANO"], "Remar": ["REMAR"], "Beautica": ["BEAUTICA"], "Maskit": ["MASKIT"],
+    "Claresta": ["CLARESTA"], "Birth Beyond": ["BIRTH"], "Rose All Day": ["ROSE ALL DAY"],
+}
+
+SALES_MAPPING = {
+    "WIRA VG": "WIRA", "WIRA - VG": "WIRA", "WIRA VLAGIO": "WIRA",
+    "WIRA HONOR": "WIRA", "WIRA - HONOR": "WIRA", "WIRA HR": "WIRA",
+    "WIRA SYB": "WIRA", "WIRA - SYB": "WIRA",
+    "WIRA SOMETHINC": "WIRA", "PMT-WIRA": "WIRA", 
+    "WIRA ELIZABETH": "WIRA", "WIRA WALNUTT": "WIRA", "WIRA ELZ": "WIRA",
+    "HAMZAH VG": "HAMZAH", "HAMZAH - VG": "HAMZAH",
+    "HAMZAH HONOR": "HAMZAH", "HAMZAH - HONOR": "HAMZAH",
+    "HAMZAH SYB": "HAMZAH", "HAMZAH AV": "HAMZAH", "HAMZAH AINIE": "HAMZAH",
+    "HAMZAH RAMADANI": "HAMZAH", "HAMZAH RAMADANI ": "HAMZAH", "HAMZA AV": "HAMZAH",
+    "FERI VG": "FERI", "FERI - VG": "FERI",
+    "FERI HONOR": "FERI", "FERI - HONOR": "FERI",
+    "FERI THAI": "FERI", "FERI - INESIA": "FERI",
+    "YOGI TF": "YOGI", "YOGI THE FACE": "YOGI", 
+    "YOGI YCM": "YOGI", "YOGI MILANO": "YOGI", "MILANO - YOGI": "YOGI", "YOGI REMAR": "YOGI",
+    "GANI CASANDRA": "GANI", "GANI REN": "GANI", "GANI R & L": "GANI", "GANI TF": "GANI", 
+    "GANI - YCM": "GANI", "GANI - MILANO": "GANI", "GANI - HONOR": "GANI", "GANI - VG": "GANI", 
+    "GANI - TH": "GANI", "GANI INESIA": "GANI", "GANI - KSM": "GANI", "SSL - GANI": "GANI",
+    "GANI ELIZABETH": "GANI", "GANI WALNUTT": "GANI",
+    "MITHA MASKIT": "MITHA", "MITHA RAD": "MITHA", "MITHA CLA": "MITHA", "MITHA OT": "MITHA",
+    "MAS - MITHA": "MITHA", "SSL BABY - MITHA ": "MITHA", "SAVIOSA - MITHA": "MITHA", "MITHA ": "MITHA",
+    "LYDIA KITO": "LYDIA", "LYDIA K": "LYDIA", "LYDIA BB": "LYDIA", "LYDIA - KITO": "LYDIA",
+    "NOVI AINIE": "NOVI", "NOVI AV": "NOVI", "NOVI DAN RAFFI": "NOVI", "NOVI & RAFFI": "NOVI", 
+    "RAFFI": "NOVI", "RAFI": "NOVI", "RAPI": "NOVI", "RAPI AV":"NOVI",
+    "ROZY AINIE": "ROZY", "ROZY AV": "ROZY",
+    "DANI AINIE": "DANI", "DANI AV": "DANI", "DANI SEKAWAN": "DANI",
+    "MADONG - MYKONOS": "MADONG", "MADONG - MAJU": "MADONG", "MADONG MYK": "MADONG",
+    "RISKA AV": "RISKA", "RISKA BN": "RISKA", "RISKA CRS": "RISKA", "RISKA E-WL": "RISKA", 
+    "RISKA JV": "RISKA", "RISKA REN": "RISKA", "RISKA R&L": "RISKA", "RISKA SMT": "RISKA", 
+    "RISKA ST": "RISKA", "RISKA SYB": "RISKA", "RISKA - MILANO": "RISKA", "RISKA TF": "RISKA",
+    "RISKA - YCM": "RISKA", "RISKA HONOR": "RISKA", "RISKA - VG": "RISKA", "RISKA TH": "RISKA", 
+    "RISKA - INESIA": "RISKA", "SSL - RISKA": "RISKA", "SKIN - RIZKA": "RISKA", 
+    "ADE CLA": "ADE", "ADE CRS": "ADE", "GLOOW - ADE": "ADE", "ADE JAVINCI": "ADE", "ADE JV": "ADE",
+    "ADE SVD": "ADE", "ADE RAM PUTRA M.GIE": "ADE", "ADE - MLEN1": "ADE", "ADE NEWLAB": "ADE", "DORS - ADE": "ADE",
+    "FANDI - BONAVIE": "FANDI", "DORS- FANDI": "FANDI", "FANDY CRS": "FANDI", "FANDI AFDILLAH": "FANDI", 
+    "FANDY WL": "FANDI", "GLOOW - FANDY": "FANDI", "FANDI - GOUTE": "FANDI", "FANDI MG": "FANDI", 
+    "FANDI - NEWLAB": "FANDI", "FANDY - YCM": "FANDI", "FANDY YLA": "FANDI", "FANDI JV": "FANDI", "FANDI MLEN": "FANDI",
+    "NAUFAL - JAVINCI": "NAUFAL", "NAUFAL JV": "NAUFAL", "NAUFAL SVD": "NAUFAL", 
+    "RIZKI JV": "RIZKI", "RIZKI SVD": "RIZKI", 
+    "SAHRUL JAVINCI": "SYAHRUL", "SAHRUL TF": "SYAHRUL", "SAHRUL JV": "SYAHRUL", "GLOOW - SAHRUL": "SYAHRUL",
+    "SANTI BONAVIE": "SANTI", "SANTI WHITELAB": "SANTI", "SANTI GOUTE": "SANTI",
+    "DWI CRS": "DWI", "DWI NLAB": "DWI", 
+    "ASWIN ARTIS": "ASWIN", "ASWIN AI": "ASWIN", "ASWIN Inc": "ASWIN", "ASWIN INC": "ASWIN", "ASWIN - ARTIST INC": "ASWIN",
+    "BASTIAN CASANDRA": "BASTIAN", "SSL- BASTIAN": "BASTIAN", "SKIN - BASTIAN": "BASTIAN", 
+    "BASTIAN - HONOR": "BASTIAN", "BASTIAN - VG": "BASTIAN", "BASTIAN TH": "BASTIAN", 
+    "BASTIAN YL": "BASTIAN", "BASTIAN YL-DIO CAT": "BASTIAN", "BASTIAN SHMP": "BASTIAN", "BASTIAN-DIO 45": "BASTIAN",
+    "SSL - DEVI": "DEVI", "SKIN - DEVI": "DEVI", "DEVI Y- DIOSYS CAT": "DEVI",
+    "DEVI YL": "DEVI", "DEVI SHMP": "DEVI", "DEVI-DIO 45": "DEVI", "DEVI YLA": "DEVI",
+    "SSL- BAYU": "BAYU", "SKIN - BAYU": "BAYU", "BAYU-DIO 45": "BAYU", "BAYU YL-DIO CAT": "BAYU", 
+    "BAYU SHMP": "BAYU", "BAYU YL": "BAYU", 
+    "HABIBI - FZ": "HABIBI", "HABIBI SYB": "HABIBI", "HABIBI TH": "HABIBI", 
+    "GLOOW - LISMAN": "LISMAN", "LISMAN - NEWLAB": "LISMAN", 
+    "WILLIAM BTC": "WILLIAM", "WILLI - ROS": "WILLIAM", "WILLI - WAL": "WILLIAM",
+    "RINI JV": "RINI", "RINI SYB": "RINI", 
+    "FAUZIAH CLA": "FAUZIAH", "FAUZIAH ST": "FAUZIAH", 
+    "MARIANA CLIN": "MARIANA", "JAYA - MARIANA": "MARIANA"
+}
+
+# ==========================================
 # 3. CORE LOGIC
 # ==========================================
 
-# --- SECURITY FEATURE: AUDIT LOGGING ---
 def log_activity(user, action):
     log_file = 'audit_log.csv'
-    # Menggunakan WIB untuk Log
     timestamp = get_current_time_wib().strftime("%Y-%m-%d %H:%M:%S")
     new_log = pd.DataFrame([[timestamp, user, action]], columns=['Timestamp', 'User', 'Action'])
     
@@ -74,11 +252,8 @@ def log_activity(user, action):
         new_log.to_csv(log_file, index=False)
     else:
         new_log.to_csv(log_file, mode='a', header=False, index=False)
-# ---------------------------------------
 
-# --- FITUR: HARDCODED TIMEZONE WIB (GMT+7) ---
 def get_current_time_wib():
-    # Memaksa program menggunakan zona waktu Asia/Jakarta (WIB)
     return datetime.datetime.now(pytz.timezone('Asia/Jakarta'))
 
 def format_idr(value):
@@ -87,19 +262,10 @@ def format_idr(value):
     except:
         return "Rp 0"
 
-# --- SECURITY: DAILY TOKEN GENERATOR (12-HOUR ROTATION) ---
 def generate_daily_token():
-    """
-    Membuat token 4 digit unik yang berubah setiap 12 jam (AM/PM) mengikuti waktu WIB.
-    Rumus: Hash(Tanggal + AM/PM + Secret Salt) -> Ambil 4 digit angka
-    """
     secret_salt = "RAHASIA_PERUSAHAAN_2025" 
-    
-    # Ambil waktu WIB saat ini
-    now_wib = get_current_time_wib()
-    
-    # Format string kunci: YYYY-MM-DD-AM atau YYYY-MM-DD-PM
-    time_key = now_wib.strftime("%Y-%m-%d-%p")
+    today_str = get_current_time_wib().strftime("%Y-%m-%d")
+    time_key = get_current_time_wib().strftime("%Y-%m-%d-%p")
     raw_string = f"{time_key}-{secret_salt}"
     
     hash_object = hashlib.sha256(raw_string.encode())
@@ -107,7 +273,6 @@ def generate_daily_token():
     
     numeric_filter = filter(str.isdigit, hex_dig)
     numeric_string = "".join(numeric_filter)
-    
     token = numeric_string[:4].ljust(4, '0')
     return token
 
@@ -116,12 +281,9 @@ def render_custom_progress(title, current, target):
     pct = (current / target) * 100
     visual_pct = min(pct, 100)
     
-    if pct < 50:
-        bar_color = "linear-gradient(90deg, #e74c3c, #c0392b)" 
-    elif 50 <= pct < 80:
-        bar_color = "linear-gradient(90deg, #f1c40f, #f39c12)" 
-    else:
-        bar_color = "linear-gradient(90deg, #2ecc71, #27ae60)" 
+    if pct < 50: bar_color = "linear-gradient(90deg, #e74c3c, #c0392b)" 
+    elif 50 <= pct < 80: bar_color = "linear-gradient(90deg, #f1c40f, #f39c12)" 
+    else: bar_color = "linear-gradient(90deg, #2ecc71, #27ae60)" 
     
     st.markdown(f"""
     <div style="margin-bottom: 20px; background-color: #fcfcfc; padding: 15px; border-radius: 12px; border: 1px solid #eee; box-shadow: 0 1px 3px rgba(0,0,0,0.05);">
@@ -604,16 +766,20 @@ def main_dashboard():
             master_pivot = master_pivot.reset_index()
             master_pivot = master_pivot.rename(columns={'Nama Outlet': 'Nama Customer'})
 
-            # --- MENGAKTIFKAN FILTER KOLOM GLOBAL (AGGRID & STREAMLIT NATIVE) ---
             if AGGRID_AVAILABLE:
                 gb = GridOptionsBuilder.from_dataframe(master_pivot)
                 gb.configure_pagination(paginationAutoPageSize=True)
                 gb.configure_side_bar()
                 
-                # FITUR TAMBAHAN: Mengaktifkan Filter secara Global untuk AgGrid
-                # Ini akan membuat setiap kolom (Kode Customer, Nama Customer, Kota, hingga bulan-bulan) bisa di-filter
-                gb.configure_default_column(filter=True, sortable=True, resizable=True)
+                # --- FITUR YANG ANDA MINTA: FILTER CHECKBOX (EXCEL-LIKE) DI SEMUA KOLOM ---
+                gb.configure_default_column(
+                    filter='agSetColumnFilter', # Menampilkan pilihan checkbox seperti Excel
+                    sortable=True, 
+                    resizable=True,
+                    floatingFilter=True # Menampilkan kotak pencarian persis di bawah header
+                )
                 
+                # Format khusus untuk kolom angka agar menjadi Rupiah
                 num_cols = list(bulan_indo.values()) + ['Total Penjualan']
                 for col in num_cols:
                     gb.configure_column(col, type=["numericColumn","numberColumnFilter"], valueFormatter="x.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', minimumFractionDigits: 0})")
@@ -622,7 +788,6 @@ def main_dashboard():
                 AgGrid(master_pivot, gridOptions=gridOptions, enable_enterprise_modules=True, height=500, theme='alpine')
             else:
                 format_dict = {col: "Rp {:,.0f}" for col in list(bulan_indo.values()) + ['Total Penjualan']}
-                # Jika aggrid tidak terinstall, gunakan DataFrame bawaan Streamlit
                 st.dataframe(master_pivot.style.format(format_dict), use_container_width=True, hide_index=True)
         else:
             st.info("Data Kosong.")
